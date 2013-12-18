bfdc1ddfad38fe0089cc278196c2b4d592f496c6
Mike Cheng <xuehung@gmail.com>
2013-12-19 02:08:12 +0800

    added smartfile support

--- app/controllers/CrdSrcController.java
+++ app/controllers/CrdSrcController.java
@@ -1,7 +1,12 @@
 package controllers;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.json.*;
+import com.smartfile.api.*;
 
+import java.io.StringWriter;
+import java.io.InputStream;
 import java.io.File;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +25,8 @@ import views.html.*;
 import models.CSRecord;
 
 public class CrdSrcController extends Controller {
+    private static String sfKey = "Vb5EI1bHvLfCvJ3tRZ9z6GT559X6Ec";
+    private static String sfPwd = "dveZC2HDtBB1rwk6FBl4JL944lSCJK";
 
     public static Result uploadForm() {
         return ok(upload.render()); 
@@ -53,6 +60,27 @@ public class CrdSrcController extends Controller {
 	r.picThreeName = pics[2].getFilename();
 	r.save();
 
+	String result = "";
+
+	try {
+	    BasicClient api = new BasicClient(sfKey,sfPwd);
+	    for(int i=0 ; i<pics.length ; i++) {
+		File file = pics[i].getFile();
+		// create the directory with record id
+		api.post("/path/oper/mkdir/", "/", "path="+r.id);
+
+		// upload the file to the specific directory that I just made
+		api.post("/path/data"+"/"+r.id, "/" ,pics[i].getFile());
+
+		// rename the file
+		String srcPath = r.id + "/" + pics[i].getFile().getName();
+		String dstPath = r.id + "/" + pics[i].getFilename();
+		api.post("/path/oper/rename/", "/", "src="+srcPath+"&dst="+dstPath);
+	    }
+	} catch (Exception e) {
+	    return ok(e.toString());
+	}
+
 	// create the directory
 	File dir = new File(playPath+"/public/csimg/"+r.id);
 	try {
@@ -62,13 +90,12 @@ public class CrdSrcController extends Controller {
 	}
 
 	// Move the images
-	String result = "";
 	try {
 	    for(int i=0 ; i<pics.length ; i++) {
 		File srcFile = pics[i].getFile();
 		File dstFile = new File(dir.toString() + "/" + pics[i].getFilename()); // directory path + / + file name
 		FileUtils.moveFile(srcFile, dstFile);
-		result += (dstFile.toString() + ":");
+		//result += (dstFile.toString() + ":");
 	    }
 	} catch (Exception e) {
 	    return ok("Failed to move files");
@@ -81,9 +108,24 @@ public class CrdSrcController extends Controller {
 
     @play.db.jpa.Transactional
     public static Result inputForm() {
+	// get the record
 	Query query = JPA.em().createQuery("SELECT r FROM CSRecord r");
 	List<CSRecord> rList = query.getResultList();
-	return ok(CSInputForm.render(rList.get(rList.size()-1))); 
+	CSRecord r = rList.get(rList.size()-1);
+	
+	String result = "";
+	//get the url of the directory of images
+	try {
+	    BasicClient api = new BasicClient(sfKey,sfPwd);
+	    InputStream is = api.post("/link/", "/", "path="+r.id+"&read=true&list=true");
+	    StringWriter writer = new StringWriter();
+	    IOUtils.copy(is, writer);
+	    result = writer.toString();
+	    JSONObject jsonObj = new JSONObject(result);
+	    result = (String)jsonObj.get("href");
+	} catch(Exception e) {
+	}
+	return ok(CSInputForm.render(r, result)); 
     }
 
 }
